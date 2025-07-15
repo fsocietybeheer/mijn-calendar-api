629b6b3a5bd1fe38e3b873d9e14438f65402d66a
fsocietybeheer <info@fsocietybeheer.nl>
2025-07-15 09:40:35 +0200

    Update prices.js

--- api/prices.js
+++ api/prices.js
@@ -1,6 +1,16 @@
 import { google } from 'googleapis';
 
 export default async function handler(req, res) {
+  // CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   console.log("prices.js endpoint aangeroepen");
 
   try {
